bbee6369a55024f7d491043d4412b2d89e8b6b47
Otoniel Duarte <otonielduarte2@gmail.com>
2020-12-06 08:37:57 -0300

    routes and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return <></>;
-};
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Reposiroty: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+export default Reposiroty;
